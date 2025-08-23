14d22aadf0af048d7264ca799a1958f45828dbac
Zeyad <zeyadw526@gmail.com>
2025-08-23 21:38:02 +0300

    setup API

---
+++ src/config/env.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+import { config } from 'dotenv';
+
+config({ quiet: true });
+
+export default z
+  .object({
+    PORT: z.coerce.number().default(3000),
+    NODE_ENV: z.enum(['development', 'production']).default('development'),
+
+    LOG_LEVEL: z
+      .enum(['error', 'warn', 'info', 'http', 'verbose', 'debug', 'silly'])
+      .default('info'),
+  })
+  .parse(process.env);
